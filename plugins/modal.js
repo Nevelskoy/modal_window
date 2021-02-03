ad212e525903b8e94e386e5a58afedba3a62ef92
Nevelskoy <s.alex1985@mail.ru>
2021-02-03 20:55:05 +0300

    update open/close JS

--- plugins/modal.js
+++ plugins/modal.js
@@ -25,13 +25,21 @@ function _createModal(options) {
 
 $.modal = function(options) {
   const $modal = _createModal(options)
+  const ANIMATION_SPEED = 200
+  let closing = false
 
   return {
     open() {
-      $modal.classList.add('open')
+      !closing && $modal.classList.add('open')
     },
     close() {
+      closing = true
       $modal.classList.remove('open')
+      $modal.classList.add('hide')
+      setTimeout(() => {
+        $modal.classList.remove('hide')
+        closing = false
+      }, ANIMATION_SPEED)
     },
     destroy() {}
   }
