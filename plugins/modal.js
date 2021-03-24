433f2472b6526ab2eeccc64ddfa8e7f80d0698fc
Nevelskoy <s.alex1985@mail.ru>
2021-03-24 21:19:37 +0300

    modal plugin

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,46 +1,40 @@
 function _createModal(options) {
-  const modal = document.createElement('div')
-  modal.classList.add('smodal')
-  modal.insertAdjacentHTML('afterbegin',`
+  const modal = document.createElement('div');
+  modal.classList.add('smodal');
+  modal.insertAdjacentHTML(
+    'afterbegin',
+    `
       <div class="modal-overlay">
         <div class="modal-window">
           <div class="modal-header">
-              <span class="modal-title">Modal title</span>
-              <span class="modal-close">&times;</span>
-            </div>
-            <div class="modal-body">
-            <p>Lorem ipsum dolor sit.</p>
-            <p>Lorem ipsum dolor sit.</p>
+            <span class="modal-title">Modal title</span>
+            <span class="modal-close">&times;</span>
           </div>
+          <div class="modal-body"></div>
+          <p>Lorem ipsum dolor sit.</p>
+          <p>Lorem ipsum dolor sit.</p>
           <div class="modal-footer">
             <button>Ok</button>
             <button>Cancel</button>
           </div>
         </div>
-      </div>
-  `)
-  document.body.appendChild(modal)
-  return modal
+      </div> 
+    `
+  );
+  document.body.appendChild(modal);
+  return modal;
 }
 
-$.modal = function(options) {
-  const $modal = _createModal(options)
-  const ANIMATION_SPEED = 200
-  let closing = false
+$.modal = function (options) {
+  const $modal = _createModal(options);
 
   return {
     open() {
-      !closing && $modal.classList.add('open')
+      $modal.classList.add('open');
     },
     close() {
-      closing = true
-      $modal.classList.remove('open')
-      $modal.classList.add('hide')
-      setTimeout(() => {
-        $modal.classList.remove('hide')
-        closing = false
-      }, ANIMATION_SPEED)
+      $modal.classList.remove('close');
     },
-    destroy() {}
-  }
-}
+    destroy() {},
+  };
+};
